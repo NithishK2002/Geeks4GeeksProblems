9f6aa211073588c17b8b64fe0976fbde520874ed
NithishK2002 <93331531+NithishK2002@users.noreply.github.com>
2023-06-21 09:53:45 +0530

    Create Reverse Coding.java

---
+++ Reverse Coding.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(S[0]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfNaturals(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        int num = n * (n + 1);
+        int res = num/2;
+        int checkValue = 1000000007;
+        res = res % checkValue;
+        return res;
+    }
+};
