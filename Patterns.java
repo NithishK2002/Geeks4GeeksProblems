9ba0bca95357c865781012541c7a1a96336074b8
NithishK2002 <93331531+NithishK2002@users.noreply.github.com>
2023-07-09 17:12:00 +0530

    Create Patterns.java

---
+++ Patterns.java
@@ -0,0 +1,87 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+        pattern14(5);
+    }
+    static void pattern10(int n)
+    {
+        int j=1;
+        for(int i=1;i<2*n;i++)
+        {
+            if(i<=n)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else
+            {
+                for(j=i;j<2*n;j++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    static void pattern11(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            int res=i%2==0?0:1;
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(res+" ");
+                res=res==0?1:0;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern12(int n)
+    {
+        
+        int space=(n-1)*4;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            for(int j=1;j<=space;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            space-=4;
+            System.out.println();
+        }
+        
+    }
+    static void pattern13(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(i+1+j+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern14(int n)
+    {
+        int temp=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print(temp++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
