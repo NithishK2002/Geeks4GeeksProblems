1a7a2c023dad6c06e17b6458e6b0ec8816902cfd
NithishK2002 <93331531+NithishK2002@users.noreply.github.com>
2023-07-12 22:48:09 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -26,6 +26,7 @@ public class MyClass {
             }
         }
     }
+    
     static void pattern11(int n)
     {
         for(int i=1;i<=n;i++)
@@ -93,6 +94,98 @@ public class MyClass {
             {
                 System.out.print((char)(j+64)+" ");
             }
+            System.out.println();
+        }
+    }
+    static void pattern16(int n)
+    {
+        for(int i=65;i<65+n;i++)
+        {
+            for(int j=65;j<=i;j++)
+            {
+                System.out.print((char)i+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern17(int n)
+    {
+        int start=65;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j<n;j++)
+                System.out.print(" ");
+            for(int j=1;j<=i;j++)
+                System.out.print((char)start++);
+            start--;
+            for(int j=i;j>1;j--)
+                System.out.print((char)--start);
+            System.out.println();
+                
+        }
+    }
+    static void pattern18(int n)
+    {
+        for(int i=n;i>=1;i--)
+        {
+            int start=64+i;
+            for(int j=0;j<n-(i-1);j++)
+                System.out.print((char)start++);
+            System.out.println();
+        }
+    }
+    static void pattern19(int n)
+    {
+        int space=0;
+        
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+                System.out.print("* ");
+            for(int j=0;j<space;j++)
+                System.out.print(" ");
+            space+=4;
+            for(int j=i;j>=1;j--)
+                System.out.print("* ");
+            System.out.println();
+        }
+        space-=4;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+                System.out.print("* ");
+            for(int j=0;j<space;j++)
+                System.out.print(" ");
+            space-=4;
+            for(int j=i;j>=1;j--)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+    static void pattern20(int n)
+    {
+        int space=(2*n)-2;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int j=0;j<space;j++)
+                System.out.print(" ");
+            space-=2;
+            for(int j=i;j>=1;j--)
+                System.out.print("*");
+            System.out.println();
+        }
+        space+=4;
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int j=0;j<space;j++)
+                System.out.print(" ");
+            space+=2;
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
             System.out.println();
         }
     }
