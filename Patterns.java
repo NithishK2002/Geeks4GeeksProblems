80df9b827b971e40be12722101d67f42f356cc7f
NithishK2002 <93331531+NithishK2002@users.noreply.github.com>
2023-07-12 23:16:46 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -189,4 +189,28 @@ public class MyClass {
             System.out.println();
         }
     }
+    static void pattern21(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(i==1 || i==n)
+            {
+                for(int j=1;j<=n;j++)
+                    System.out.print("*");
+                // System.out.println();
+            }
+            
+            else
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    if(j==1 || j==n)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
